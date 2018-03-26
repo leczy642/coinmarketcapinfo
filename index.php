737798df34866d2d5907cf5a84f0161bd01bb175
leczy <leczy642@yahoo.com>
2018-03-26 10:46:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -24,6 +24,12 @@ if(isset($_POST['submit'])){
 			$coin_price=$coin->price_usd; // set the price here instead
 			$coin_rank = $coin -> rank; //display the rank
 			$coin_available_supply = $coin -> available_supply; //set the available supply
+			$total_24h_volume_usd = $coin -> total_24h_volume_usd;
+			$market_cap_usd = $coin -> market_cap_usd;
+			$total_supply = $coin -> total_supply;
+			$percent_change_1h = $coin -> percent_change_1h;
+			$percent_change_24h = $coin -> percent_change_24h;
+			$percent_change_7d = $coin -> percent_change_7d;
 		}
 				//displaying the coin information
 			echo ("<b>---COINMARKETCAP INFO---</b><br/>");
@@ -31,7 +37,12 @@ if(isset($_POST['submit'])){
 			echo ("SBD PRICE -->".$coin_price."<br/>");
 			echo ("RANK -->".$coin_rank."<br/>");
 			echo ("AVAILABLE SUPPLY -->".$coin_available_supply."<br/>");
-
+			echo ("TOTAL 24 HOUR VOLUME -->".$total_24h_volume_usd."<br/>");
+			echo ("MARKET CAP IN USD -->".$market_cap_usd."<br/>");
+			echo ("TOTAL SUPPLY -->".$total_supply."<br/>");
+			echo ("PERCENT CHANGE 1 HOUR -->".$percent_change_1h."<br/>");
+			echo ("PERCENT CHANGE 24 HOURS -->".$percent_change_24h."<br/>");
+			echo ("PERCENT CHANGE 7 DAYS -->".$percent_change_7d."<br/>");
 	}
 
 ?>
